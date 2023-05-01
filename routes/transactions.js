ee4adf115f3f5b64855bf5c6f761ff555fe09297
NITHISHguviii <nithishkumarv0612@gmail.com>
2023-05-01 15:35:57 +0530

    routeschanges

--- routes/transactions.js
+++ routes/transactions.js
@@ -6,10 +6,10 @@ const { addIncome , getIncomes , deleteIncome} = require('../controllers/income'
 const router = require('express').Router();
 
 router.post('/add-income', addIncome)
-.get('/get-incomes', getIncomes)
-.delete('/delete-income/:id', deleteIncome)
-.post('/add-expense', addExpense)
-.get('/get-expenses', getExpense)
+router.get('/get-incomes', getIncomes)
+router.delete('/delete-income/:id', deleteIncome)
+router.post('/add-expense', addExpense)
+router.get('/get-expenses', getExpense)
     .delete('/delete-expense/:id', deleteExpense)
 
 module.exports = router
