b63727dbf11790ebe181d4ff648efae98959942d
NITHISHguviii <nithishkumarv0612@gmail.com>
2023-05-01 15:43:27 +0530

    exportschanges

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,19 +1,23 @@
+const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+} = require("../controllers/expense");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/income");
 
-const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
-const { addIncome , getIncomes , deleteIncome} = require('../controllers/income');
+const router = require("express").Router();
 
+router.post("/add-income", addIncome);
+router.get("/get-incomes", getIncomes);
+router.delete("/delete-income/:id", deleteIncome);
+router.post("/add-expense", addExpense);
+router.get("/get-expenses", getExpense);
+router.delete("/delete-expense/:id", deleteExpense);
 
-const router = require('express').Router();
-
-router.post('/add-income', addIncome)
-router.get('/get-incomes', getIncomes)
-router.delete('/delete-income/:id', deleteIncome)
-router.post('/add-expense', addExpense)
-router.get('/get-expenses', getExpense)
-    .delete('/delete-expense/:id', deleteExpense)
-
-module.exports = router
-
-
+module.exports = router;
 
 //    http://localhost:5000/api/v1/get-incomes
